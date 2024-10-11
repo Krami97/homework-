5e702377da1391b6fee41de5c7ad06de9d130be1
Krami97 <tin.kramaric@gmail.com>
2024-10-11 15:01:17 +0200

    DZ3 - Zadatak 1: Brojevi od 1 do N

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Ispis brojva od 1 do N");
+        System.out.println("Unesite zadnji broj N=");
+        int N = scanner.nextInt();
 
-        System.out.println("unesi prvi cjeli broj: ");
-        Integer number1 = scanner.nextInt();
-        System.out.println("unesi drugi cijeli broj: ");
-        Integer number2 = scanner.nextInt();
-        System.out.println("odaberi matematičku operaciju: \n1 zbrajanje\n2 oduzimanje\n3 množenje\n4 djeljenje");
-        int operator = scanner.nextInt();
-
-
-        if (operator == 1){
-            System.out.println(number1 + " + " + number2 + " = " +  (number2 + number1));
-        } else if (operator == 2) {
-            System.out.println(number1 + " - " + number2 + " = " +  (number2 - number1));
-        } else if (operator == 3) {
-            System.out.println(number1 + " * " + number2 + " = " +  (number2 * number1));
-        } else if (operator == 4) {
-            System.out.println(number1 + " / " + number2 + " = " +  ((float) number1 / number2));
+        for(int i=1;i<N;i++){
+            System.out.println(i);
         }
+
+
+
+
 
     }
 }
