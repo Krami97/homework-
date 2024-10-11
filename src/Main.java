0b5fb16265bb84c842bb7473542566c4b4da0e8e
Krami97 <tin.kramaric@gmail.com>
2024-10-11 15:12:58 +0200

    DZ3 - Zadatak 2: Brojanje znamenki broja

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,21 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ispis brojva od 1 do N");
-        System.out.println("Unesite zadnji broj N=");
+        System.out.println("Saznaj broj znamenki nekog broja");
+        System.out.println("Unesite neki cijeli broj:");
         int N = scanner.nextInt();
-
-        for(int i=1;i<N;i++){
-            System.out.println(i);
+        int decimalPlaces = 0;
+        if (N<0){
+            N*=-1;
         }
 
+        while (N>0){
+            N/=10;
+            decimalPlaces++;
 
+        }
+
+        System.out.println("Uneseni broj ima "+ decimalPlaces + " znamenke");
 
 
 
