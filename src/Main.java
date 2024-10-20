c8d4190bfb561662d14b8ff28deaa17c4918523b
Krami97 <tin.kramaric@gmail.com>
2024-10-20 16:24:14 +0200

    DZ5 -ZAD1 Zamjena njamanjeg i največeg broja u listi

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,35 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        int najveciBroj;
-        System.out.println("Koliko brojeva želite unjesti?");
-        int velicinaPolja = scanner.nextInt();
-        int[] polje = new int[velicinaPolja];
 
-        for(int i = 0;i<polje.length;i++){
-            System.out.printf("Unesi %d broj\n",i+1);
-            polje[i]= scanner.nextInt();
+        List<Integer> numberList = new ArrayList<>();
+
+
+        for(int i = 0 ; i<10;i++){
+            System.out.printf("Unesi %d. broj:\n",i+1);
+            numberList.add(scanner.nextInt());
         }
-        najveciBroj = polje[0];
+        //Pronaženje najmanjeg i največeg broja u listi
+        int minimum = Collections.min(numberList);
+        int maximum = Collections.max(numberList);
+        // Pronaženje indexa najmanjeg i največeg broja u listi
+        int indexOfMinimum = numberList.indexOf(minimum);
+        int indexOfMaximum = numberList.indexOf(maximum);
+        // zamjena najmanjeg i največeg broja u listi
+        numberList.set(indexOfMinimum,maximum);
+        numberList.set(indexOfMaximum,minimum);
+        // i
+        System.out.println("Nova lista brojeva");
+        for (Integer i : numberList) {
+            System.out.printf("%d,",i);
 
-        for (int i : polje) {
-            if (i > najveciBroj){
-                najveciBroj = i;
-            }
         }
-        System.out.printf("od svih unesenih brojeva najveci je %d",najveciBroj);
-
-
-
     }
+
 }
