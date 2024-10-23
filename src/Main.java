a88379b86bb9ce60611bfbfe5496b0f3e1f588c9
Krami97 <tin.kramaric@gmail.com>
2024-10-23 20:05:37 +0200

    Predavanje 23.10.2024 Zadatak prosjek bez min & max

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,29 +12,33 @@ public class Main {
         System.out.println("Koliko elemenata želite unjeti u listu ?:");;
         int velicinaListe = scanner.nextInt();
         List<Integer> brojevi = new ArrayList<>();
-        boolean duplicPostoji = false;
+
+
+
 
         for(int i = 0; i< velicinaListe ; i++){
             System.out.printf("Upišite %d broj:\n",1+i);
             brojevi.add(scanner.nextInt());
         }
 
+        int maxbroj = brojevi.get(0);
+        int minbroj = brojevi.get(0);
+        int suma = 0;
+
         for (Integer i : brojevi) {
-            int duplic = 0;
-
-            for (Integer integer : brojevi) {
-                if (i.equals(integer)) {
-                    duplic++;
-                }
-
+            suma+=i;
+            if(i <=minbroj){
+                minbroj=i;
             }
-            if (duplic >= 2){
-                duplicPostoji = true;
-                break;
+            if(i >=maxbroj){
+                maxbroj=i;
             }
-
         }
-        System.out.println("duplic postoji:" + duplicPostoji);
+
+        float prosjek = (float) (suma-maxbroj-minbroj)/(brojevi.size()-2);
+        System.out.printf("Prosjek svih brojeva iz liste bez maksa i mina je %f",prosjek);
+
+
 
 
     }
