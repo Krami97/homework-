6772440c2b0d278a69436b1492bb7f158aac59bd
Krami97 <tin.kramaric@gmail.com>
2024-10-23 19:50:49 +0200

    Predavanje 23.10.2024 Zadtak duplic

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,40 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numberList = new ArrayList<>();
-        List<Integer> listaBrojaca = new ArrayList<>();
 
-        // inicializacija liste brojaca na nulu
-        for (int i = 0; i < 10; i++) {
-            listaBrojaca.add(0);
+        System.out.println("Koliko elemenata želite unjeti u listu ?:");;
+        int velicinaListe = scanner.nextInt();
+        List<Integer> brojevi = new ArrayList<>();
+        boolean duplicPostoji = false;
+
+        for(int i = 0; i< velicinaListe ; i++){
+            System.out.printf("Upišite %d broj:\n",1+i);
+            brojevi.add(scanner.nextInt());
         }
-        // unos brojeva od starane korisnika
-        for(int i = 0 ; i<10 ;i++){
-            System.out.printf("Unesi %d. broj:\n",i+1);
-            numberList.add(scanner.nextInt());
-        }
-        // provjera koliko se koji borj ponaclja?
-        for (int i = 0 ;i<10;i++) {
-            int trenutniBroj = numberList.get(i);
-            for (Integer integer : numberList) {
-                if(trenutniBroj== integer){
-                    listaBrojaca.set(i,listaBrojaca.get(i)+1);
+
+        for (Integer i : brojevi) {
+            int duplic = 0;
+
+            for (Integer integer : brojevi) {
+                if (i.equals(integer)) {
+                    duplic++;
                 }
 
             }
+            if (duplic >= 2){
+                duplicPostoji = true;
+                break;
+            }
 
         }
-        int maxTimes = Collections.max(listaBrojaca);
-        int indexOfMax = listaBrojaca.indexOf(maxTimes);
-        int maxTimesNumber = numberList.get(indexOfMax);
-        if(maxTimes>1){
-            System.out.printf("Najcesce unesen broj je %d i on je unesen %d puta",maxTimesNumber,maxTimes);
-        }else{
-            System.out.println("Nijedan unsesni borj nije unesen vise od jedanput");
-        }
-}
+        System.out.println("duplic postoji:" + duplicPostoji);
+
+
+    }
 }
