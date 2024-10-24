115485722423d733b6c094ab3fb96490e35d37f9
Krami97 <tin.kramaric@gmail.com>
2024-10-24 20:44:15 +0200

    Predavanje 24-10-2024 funkcija za uzlazno soritiranu listu

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-
-        System.out.println("Koliko elemenata želite unjeti u listu ?:");;
-        int velicinaListe = scanner.nextInt();
         List<Integer> brojevi = new ArrayList<>();
 
+        System.out.println("koliko brojeva zelite unjeti:");
+        int brojUnosa = scanner.nextInt();
 
-
-
-        for(int i = 0; i< velicinaListe ; i++){
-            System.out.printf("Upišite %d broj:\n",1+i);
+        for(int i = 0; i<brojUnosa; i++){
+            System.out.printf("unesite %d broj",i+1);
             brojevi.add(scanner.nextInt());
         }
 
-        int maxbroj = brojevi.get(0);
-        int minbroj = brojevi.get(0);
-        int suma = 0;
+        System.out.println("Lista je uzlazno sortirana je :"+ uzlaznoSoritiranaLista(brojevi));
 
-        for (Integer i : brojevi) {
-            suma+=i;
-            if(i <=minbroj){
-                minbroj=i;
-            }
-            if(i >=maxbroj){
-                maxbroj=i;
+
+
+
+    }
+    public static boolean uzlaznoSoritiranaLista(List<Integer> listaBrojeva){
+        int trenutniBroj = listaBrojeva.get(0);
+        for (Integer i : listaBrojeva) {
+            if (i>= trenutniBroj){
+                trenutniBroj=i;
+            }else{
+                return false;
             }
         }
-
-        float prosjek = (float) (suma-maxbroj-minbroj)/(brojevi.size()-2);
-        System.out.printf("Prosjek svih brojeva iz liste bez maksa i mina je %f",prosjek);
-
-
-
-
+        return true;
     }
 }
