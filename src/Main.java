1f29dfa4e60ed4d60f5ff6e77cc6b91e8fbc0551
Krami97 <tin.kramaric@gmail.com>
2024-10-29 19:52:26 +0100

    vracanje ostatka rijeseno

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -10,55 +11,20 @@ public class Main {
 
     }
     public static void kusur(String novac){
-        Float novacfloat = Float.valueOf(novac);
-        while(novacfloat>0){
-            if(novacfloat-500>=0){
-                System.out.println(500);
-                novacfloat = novacfloat - 500;
-            }else if(novacfloat-200>=0){
-                System.out.println(200);
-                novacfloat = novacfloat -200;
-            }else if(novacfloat-100>=0){
-                System.out.println(100);
-                novacfloat = novacfloat - 100;
-            }else if(novacfloat-50>=0){
-                System.out.println(50);
-                novacfloat = novacfloat - 50;
-            }else if(novacfloat-20>=0) {
-                System.out.println(20);
-                novacfloat = novacfloat - 20;
-            }else if(novacfloat-10>=0) {
-                System.out.println(10);
-                novacfloat = novacfloat - 10;
-            }else if(novacfloat-5>=0) {
-                System.out.println(5);
-                novacfloat = novacfloat - 5;
-            }else if(novacfloat-2>=0) {
-                System.out.println(2);
-                novacfloat = novacfloat - 2;
-            }else if(novacfloat-1>=0) {
-                System.out.println(1);
-                novacfloat = novacfloat - 1;
-            }else if(novacfloat-0.5>=0) {
-                System.out.println(0.5);
-                novacfloat = (float) (novacfloat - 0.5);
-            }else if(novacfloat-0.2>=0) {
-                System.out.println(0.2);
-                novacfloat = (float) (novacfloat - 0.2);
-            }else if(novacfloat-0.1>=0) {
-                System.out.println(0.1);
-                novacfloat = (float) (novacfloat - 0.1);
-            }else if(novacfloat-0.05>=0) {
-                System.out.println(0.05);
-                novacfloat = (float) (novacfloat - 0.05);
-            }else if(novacfloat-0.02>=0) {
-                System.out.println(0.02);
-                novacfloat = (float) (novacfloat - 0.02);
-            }else if(novacfloat-0.01>=0) {
-                System.out.println(0.01);
+        Double[] listaNovcanica = {500.0,200.0,100.0,50.0,20.0,10.0,5.0,2.0,1.0,0.5,0.2,0.1,0.05,0.02,0.01};
+        Double novacdouble = Double.valueOf(novac);
 
-                novacfloat = (float) (novacfloat - 0.01);
+        int i = 0;
+        System.out.println("vracamo vam:");;
+        while(novacdouble>0 && i< listaNovcanica.length){
+            novacdouble = Math.round(novacdouble * 100.0) / 100.0;
+            if (novacdouble-listaNovcanica[i]>=0){
+                System.out.println(listaNovcanica[i]);
+                novacdouble-=listaNovcanica[i];
+            }else{
+                i++;
             }
+
 
 
 
