c67f00c4de44314980e7869968d24be22d0a06d1
Krami97 <tin.kramaric@gmail.com>
2024-11-13 20:45:24 +0100

    predavanje 13.11.2024 , faktorijeli  try-catch

--- src/Knjiga.java
+++
@@ -1,9 +0,0 @@
-public interface Knjiga {
-    String getNaslov();
-    String getAutor();
-    Integer getGodinaIzdavanja();
-    Integer getBrojStraanica();
-    String getStanje();
-    void posudi();
-    void vrati();
-}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Integer unos = null;
+        long rezultat = 0;
 
+        System.out.println("Unesite broj");
+        try {
+            unos = scanner.nextInt();
+
+            rezultat = izracunajFaktorijel(unos);
+            System.out.println("Faktorijel od broja " + unos + " iznosi " + rezultat);
+        } catch (InputMismatchException e) {
+            System.out.println("Greška: Molimo unesite cijeli broj.");
+        } catch (Exception e) {
+            System.out.println("Greška: " + e.getMessage());
+        }
     }
 
-
+    public static long izracunajFaktorijel(int broj) throws Exception {
+        if (broj < 0) {
+            throw new Exception("Faktorijel nije definiran za negativne brojeve.");
+        }
+        long rezultat = 1;
+        for (int i = 1; i <= broj; i++) {
+            rezultat *= i;
+        }
+        return rezultat;
+    }
 }
+
