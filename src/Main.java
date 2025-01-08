6b53b5e5955d1f6e372dba5b25fdbc1f724d6bda
Krami97 <tin.kramaric@gmail.com>
2025-01-08 20:57:20 +0100

    zad 3 hashSet

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,52 @@
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
+
 
 public class Main {
     public static void main(String[] args) {
-        TreeSet<String> gradovi = new TreeSet<>();
-        
-        gradovi.add("Zagreb");
-        gradovi.add("Split");
-        gradovi.add("Rijeka");
-        gradovi.add("Šibenik");
-        gradovi.add("Vinkovci");
+        HashSet<String> odjelA = new HashSet<>();
+        HashSet<String> odjelB = new HashSet<>();
 
+        odjelA.add("Tin");
+        odjelA.add("Hrvoje");
+        odjelA.add("Pero");
+        odjelA.add("Ana");
 
-        for (String string : gradovi) {
+        odjelB.add("Tin");
+        odjelB.add("Marko");
+        odjelB.add("Zvonimir");
+        odjelB.add("Ana");
+
+        HashSet<String> unija = new HashSet<>();
+        unija.addAll(odjelB);
+        unija.addAll(odjelA);
+
+        System.out.println("Unija odjela:");
+        for (String string : unija) {
             System.out.println(string);
         }
 
-        System.out.println(gradovi.first());
-        System.out.println(gradovi.last());
-        gradovi.remove("Rijeka");
+        System.out.println("\nZaposlenici koji rade u oba skupa");
+        for (String string : unija) {
+            if (odjelA.contains(string) && odjelB.contains(string)){
+                System.out.println(string);
+            }
+        }
 
-        for (String string : gradovi) {
+        System.out.println("\nZaposlenici koji rade samo u jednom odjelu");
+        for (String string : unija) {
+            if (odjelA.contains(string) && odjelB.contains(string)){
+                continue;
+            }
             System.out.println(string);
         }
+
+
+
+
+
+
 
     }
 }
