f74e6a29ebe689104843201d4bc6f29dc8b2f106
Krami97 <tin.kramaric@gmail.com>
2025-01-11 11:28:04 +0100

    dz zad3

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,75 @@
-/*
-Zadatak 2: Popis jedinstvenih vrijednosti koristeći Set
-Napišite program koji:
-1.Od korisnika traži unos brojeva (razdvojenih razmakom).
-2.Koristi HashSet za izdvajanje samo jedinstvenih brojeva (bez ponavljanja).
-3.Ispisuje sve jedinstvene brojeve.
-4.Traži najveći i najmanji broj koristeći odgovarajuće metode skupa.
-
-* */
-
-
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
+/*Zadatak 3: Kategorije proizvoda koristeći Map i List
+Napišite program koji simulira sustav za upravljanje proizvodima:
+1. Koristi HashMap gdje je ključ ime kategorije (npr. "Voće", "Povrće"), a vrijednost je Lista proizvoda u toj kategoriji.
+2. Dodaje proizvode u kategorije (npr. "Jabuka" u "Voće").
+3. Omogućuje prikaz svih proizvoda unutar određene kategorije.
+4. Prikazuje sve kategorije i proizvode u njima.
+ */
 public class Main {
     public static void main(String[] args) {
+    //TODO 1. Koristi HashMap gdje je ključ ime kategorije (npr. "Voće", "Povrće"), a vrijednost je Lista proizvoda u toj kategoriji.
+    //TODO 2. Dodaje proizvode u kategorije (npr. "Jabuka" u "Voće").
+        HashMap<String, ArrayList<String>> voceIPovrce = new HashMap<>();
+        ArrayList<String> voce = new ArrayList<>(Arrays.asList("jabuka","kruška","banana","Kiwi","jagoda"));
+        ArrayList<String> povrce  = new ArrayList<>(Arrays.asList("brokula","krastavac","mrkva","paprika","račica"));
+        voceIPovrce.put("voce",voce);
+        voceIPovrce.put("povrce",povrce);
 
-    //TODO 1. Od korisnika traži unos brojeva (razdvojenih razmakom).
+    //TODO 3. Omogućuje prikaz svih proizvoda unutar određene kategorije.
+    //TODO 4. Prikazuje sve kategorije i proizvode u njima.
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite nekoliko brojeva razdvojenih razmakom");
-        String nizBrojeva = scanner.nextLine();
-        String[] brojevi  = nizBrojeva.split(" ");
-        
-    //TODO 2. Koristi HashSet za izdvajanje samo jedinstvenih brojeva (bez ponavljanja).
-        HashSet<Integer> jedinstveniBrojevi = new HashSet<>();
+        int korisnickiIzbor;
+        do{
+            System.out.println("Odaberi:");
+            System.out.println("1 za prikaz voca");
+            System.out.println("2 za prikaz povrca");
+            System.out.println("3 za pirkaz svega");
+            System.out.println("0 za izlaz");
+            korisnickiIzbor = scanner.nextInt();
 
-        for (String string : brojevi) {
-            jedinstveniBrojevi.add(Integer.valueOf(string));
+            switch (korisnickiIzbor){
+                case 1:
+                    ispisiVoce(voceIPovrce);
+                    break;
+                case 2:
+                    ispisiPovrce(voceIPovrce);
+                    break;
+                case 3:
+                    ispisSveVoceIPovrce(voceIPovrce);
+
+            }
+
+        }while(korisnickiIzbor!= 0);
+
+
+
+
+    }
+    public static void ispisiVoce(HashMap<String, ArrayList<String>> voceIPovrce){
+        System.out.println("Voće:");
+        for (String voce : voceIPovrce.get("voce")) {
+            System.out.println(voce);
         }
-
-    //TODO 3.  Ispisuje sve jedinstvene brojeve.
-        System.out.println("Jedinstveni borjevi su:");
-        for (Integer i : jedinstveniBrojevi) {
-            System.out.println(i);
+    }
+    public static void ispisiPovrce(HashMap<String, ArrayList<String>> voceIPovrce){
+        System.out.println("Povrće:");
+        for (String povrce : voceIPovrce.get("povrce")) {
+            System.out.println(povrce);
         }
-    //TODO 4. Traži najveći i najmanji broj koristeći odgovarajuće metode skupa.
-        System.out.println("\nNajveci jedinstveni borj je "+ Collections.max(jedinstveniBrojevi));
-        System.out.println("\nNajmanji jedinstveni borj je "+ Collections.min(jedinstveniBrojevi));
+    }
+    public static void ispisSveVoceIPovrce(HashMap<String, ArrayList<String>> voceIPovrce){
+        for (String string : voceIPovrce.keySet()) {
+            System.out.println(string+":");
+            for (String s : voceIPovrce.get(string)) {
+                System.out.println(s);
+            }
+        }
+    }
 
-        
-    }   
 }
 
