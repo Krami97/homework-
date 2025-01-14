8019ce57f22b13a6af1709b96b092216fcdf7eb2
Krami97 <tin.kramaric@gmail.com>
2025-01-14 19:57:13 +0100

    jdbc spajanje na bazu

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,77 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import database.DatabaseService;
 
-/*Zadatak 3: Kategorije proizvoda koristeći Map i List
-Napišite program koji simulira sustav za upravljanje proizvodima:
-1. Koristi HashMap gdje je ključ ime kategorije (npr. "Voće", "Povrće"), a vrijednost je Lista proizvoda u toj kategoriji.
-2. Dodaje proizvode u kategorije (npr. "Jabuka" u "Voće").
-3. Omogućuje prikaz svih proizvoda unutar određene kategorije.
-4. Prikazuje sve kategorije i proizvode u njima.
- */
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-    //TODO 1. Koristi HashMap gdje je ključ ime kategorije (npr. "Voće", "Povrće"), a vrijednost je Lista proizvoda u toj kategoriji.
-    //TODO 2. Dodaje proizvode u kategorije (npr. "Jabuka" u "Voće").
-        HashMap<String, ArrayList<String>> voceIPovrce = new HashMap<>();
-        ArrayList<String> voce = new ArrayList<>(Arrays.asList("jabuka","kruška","banana","Kiwi","jagoda"));
-        ArrayList<String> povrce  = new ArrayList<>(Arrays.asList("brokula","krastavac","mrkva","paprika","račica"));
-        voceIPovrce.put("voce",voce);
-        voceIPovrce.put("povrce",povrce);
-
-    //TODO 3. Omogućuje prikaz svih proizvoda unutar određene kategorije.
-    //TODO 4. Prikazuje sve kategorije i proizvode u njima.
-        Scanner scanner = new Scanner(System.in);
-        int korisnickiIzbor;
-        do{
-            System.out.println("Odaberi:");
-            System.out.println("1 za prikaz voca");
-            System.out.println("2 za prikaz povrca");
-            System.out.println("3 za pirkaz svega");
-            System.out.println("0 za izlaz");
-            korisnickiIzbor = scanner.nextInt();
-
-            switch (korisnickiIzbor){
-                case 1:
-                    ispisiVoce(voceIPovrce);
-                    break;
-                case 2:
-                    ispisiPovrce(voceIPovrce);
-                    break;
-                case 3:
-                    ispisSveVoceIPovrce(voceIPovrce);
-
-            }
-
-        }while(korisnickiIzbor!= 0);
-
+        selectExample();
+        InsertExample();
 
 
 
     }
-    public static void ispisiVoce(HashMap<String, ArrayList<String>> voceIPovrce){
-        System.out.println("Voće:");
-        for (String voce : voceIPovrce.get("voce")) {
-            System.out.println(voce);
-        }
-    }
-    public static void ispisiPovrce(HashMap<String, ArrayList<String>> voceIPovrce){
-        System.out.println("Povrće:");
-        for (String povrce : voceIPovrce.get("povrce")) {
-            System.out.println(povrce);
-        }
-    }
-    public static void ispisSveVoceIPovrce(HashMap<String, ArrayList<String>> voceIPovrce){
-        for (String string : voceIPovrce.keySet()) {
-            System.out.println(string+":");
-            for (String s : voceIPovrce.get(string)) {
-                System.out.println(s);
+    public static void InsertExample(){
+
+        Connection connection = DatabaseService.CreateConnection();
+        try{
+
+            // Izvršavanje SQL upita
+            String query = "INSET INTO Drzava(Naziv) VALUES(?)"; // Zamijenite stvarnim nazivom tablice
+
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            statement.setString(1,"Slovenija");
+            Integer result = statement.executeUpdate();
+
+            System.out.println("rezulat je :" + result);
+
+
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
     }
 
-}
+
+    public static void selectExample(){
+
+        Connection connection = DatabaseService.CreateConnection();
+        try{
+
+            // Izvršavanje SQL upita
+            String query = "SELECT * FROM Drzava"; // Zamijenite stvarnim nazivom tablice
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // Ispis rezultata
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getInt("IDDrzava"));
+                System.out.println("Naziv: " + resultSet.getString("Naziv"));
+            }
+
+            // Zatvaranje resursa
+            resultSet.close();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    }
+
 
---
+++ src/database/DatabaseService.java
@@ -0,0 +1,36 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseService {
+    // public , static class razlika je da static mormo krirati instncu
+    //Staticna klasa je kad se atirbuti ne mjenjaju dok je instaca kada se neki atibutei mogu mjenjati pa
+    // se dvije instance razlikuju u nekom segmentu
+    //staticne metode su globalne metode
+    //----------------------------------------------------------------
+    // kriranje paketa za database  i kreiranje klase za spajanje sa bazom
+    //kaoko nebi morali svaki put kreirati
+    public static Connection CreateConnection(){
+        // URL za povezivanje s bazom podataka
+        String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=AdventureWorksOBP;encrypt=true;trustServerCertificate=true";
+        String user = "root";
+        String password = "password";
+        Connection connection = null;
+        // Usposavljanje veze
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                System.out.println("Uspješno povezivanje s bazom!");
+            }
+            return connection;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
