68afa9f1e4eda00c13105a2f46eb89018de198c5
Krami97 <tin.kramaric@gmail.com>
2025-01-20 20:52:05 +0100

    JDBC transakcije predavanja zadaci

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import database.DatabaseService;
+import org.w3c.dom.ls.LSOutput;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -9,34 +13,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int odabir;
         do {
-            System.out.println("Odaberite");
-            System.out.println("1 za dodavanje autora");
-            System.out.println("2 za dodavanje knjige");
-            System.out.println("3 za dohvanje svih knjiga za zadanog autora ");
-            System.out.println("4 za aržuriranje naslova knjige");
-            System.out.println("5 za dohvacanje svhih autora bez knjiga ");
-            System.out.println("0 za izlaz iz prgrama");
+            System.out.println("Odaberite:");
+            System.out.println("1 za brisnaje racuna");
+            System.out.println("2 za novu kupovinu");
+            System.out.println("0 za izlaz");
             odabir = scanner.nextInt();
             scanner.nextLine();
 
             switch (odabir){
                 case 1:
-                    dodavanjeAutora(connection);
+                    brisanjeRacuna(connection);
                     break;
                 case 2:
-                    dodavanjeKnjige(connection);
-                    break;
-                case 3:
-                    ispisSvihKnjigaZadanogAutora(connection);
-                    break;
-                case 4:
-                    arzuriranjeNaslovaKnjige(connection);
-                    break;
-                case 5:
-                    ispisAutoraBezKnjige(connection);
-                    break;
             }
-        }while(odabir != 0);
+
+        }while(odabir!=0);
+
+
+
+
         if (connection == null) {
             try {
                 connection.close();
@@ -48,175 +43,128 @@ public class Main {
 
     }
 
-    public static void dodavanjeAutora(Connection connection){
+    public static void brisanjeRacuna(Connection connection){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ime autora:");
-        String imeAutora = scanner.nextLine();
-        System.out.println("Prezime autora:");
-        String prezimeAutora = scanner.nextLine();
-        System.out.println("Nacionalnost autora:");
-        String nacionalnostAutora = scanner.nextLine();
+        System.out.println("Unesite ID racuna koje zelite izbrisati:");
+        int idRacuna = scanner.nextInt();
 
-        String query ="INSERT INTO Autor (ime,prezime,nacionalnost) VALUES(?,?,?)";
+
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,imeAutora);
-            preparedStatement.setString(2,prezimeAutora);
-            preparedStatement.setString(3,nacionalnostAutora);
-            preparedStatement.executeUpdate();
-            preparedStatement.close();
-            System.out.println("Autor dodan\n");
+            connection.setAutoCommit(false);
 
-        }catch (SQLException e){
+            String DELETEstavkaQuery = "DELETE Stavka WHERE  RacunID = ?";
+            PreparedStatement preparedStatementStavka  =connection.prepareStatement(DELETEstavkaQuery);
+            preparedStatementStavka.setInt(1,idRacuna);
+            preparedStatementStavka.executeUpdate();
+
+            String DELETEracunQuery = "DELETE Racun WHERE  IDRacun = ?";
+            PreparedStatement preparedStatementRacun  =connection.prepareStatement(DELETEracunQuery);
+            preparedStatementRacun.setInt(1,idRacuna);
+            preparedStatementRacun.executeUpdate();
+
+            connection.commit();
+            System.out.println("Racun izbrisan!\n");
+        } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
 
         }
+
     }
-    public static void dodavanjeKnjige(Connection connection){
+    public static void novaKupovina(Connection connection){
+        // Uzimanje podatak od korisnika
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Naslov knjige:");
-        String naslovKnjige = scanner.nextLine();
-        System.out.println("godina Izdanja:");
-        int godinaIzdanja = scanner.nextInt();
-        System.out.println("ID autora:\n(unestie 0 ako zelite listu svih autora");
-        int autrorID = scanner.nextInt();
-        if (autrorID == 0){
-            ispisSvihAutora(connection);
-            System.out.println("ID autora:");
-            autrorID = scanner.nextInt();
-        }
+        System.out.println("Unesite ID kupca:");
+        int kupacId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Unestiet ID komercialista:");
+        int komercialistId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Unestiet ID Kreditne Kartice :");
+        int kreditnaKarticaID = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Uneste komentar ili 0 ako nemate komentar ");
+        String komentarRacuna = scanner.nextLine();
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String DatumIzdavanja = today.format(formatter);
 
-
-        String query ="INSERT INTO Knjiga (Naslov,GodinaIzdanja,autorID) VALUES(?,?,?)";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, naslovKnjige);
-            preparedStatement.setInt(2, godinaIzdanja);
-            preparedStatement.setInt(3,autrorID);
-            preparedStatement.executeUpdate();
-            preparedStatement.close();
-            System.out.println("Knjiga dodana\n");
-
-        }catch (SQLException e){
-            e.printStackTrace();
-
-        }
-    }
-    public static void arzuriranjeNaslovaKnjige(Connection connection){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Uneite id knjige kojoj zelite promjeniti nalsov \n(unesite 0 ako zelite ispissvih knjiga");
-        int idKnjige = scanner.nextInt();
-        if (idKnjige == 0){
-            ispisSvihKnjiga(connection);
-            System.out.println("Uneite id knjige kojoj zelite promjeniti nalsov");
-            idKnjige = scanner.nextInt();
+        HashMap<Integer, Integer> proizvodKolicina = new HashMap<>();
+        int proizvodId;
+        int kolicinaProizvoda;
+        do {
+            System.out.println("Unesite ID proizvoda:\n(Unesite 0 ako nema više proizvoda)");
+            proizvodId = scanner.nextInt();
             scanner.nextLine();
-        }
-        System.out.println("Unesite novi naslov knjige:");
-        String noviNalsovKnjige = scanner.nextLine();
+            System.out.println("Unesite kolicinu proizvoda:");
+            kolicinaProizvoda = scanner.nextInt();
+            scanner.nextLine();
+            proizvodKolicina.put(proizvodId,kolicinaProizvoda);
+        }while(proizvodId != 0);
 
-
-        String query ="UPDATE Knjiga SET Naslov = ? WHERE id = ?";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, noviNalsovKnjige);
-            preparedStatement.setInt(2, idKnjige);
-
+            connection.setAutoCommit(false);
+            // dodavanje racuna
+            String racunQuery = "INSERT INTO Racun (KupacID,KomercialistID,KreditnaKarticaID,DatumIzdavanja) VALUES(?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(racunQuery);
+            preparedStatement.setInt(1,kupacId);
+            preparedStatement.setInt(2,komercialistId);
+            preparedStatement.setInt(3,kreditnaKarticaID);
+            preparedStatement.setString(4,DatumIzdavanja);
             preparedStatement.executeUpdate();
-            preparedStatement.close();
-            System.out.println("Knjiga arzurirana\n");
 
-        }catch (SQLException e){
+
+            //dodavanje stavki
+            int racunId = DohvacanjeRacunaID(connection,DatumIzdavanja,kupacId,komercialistId);
+
+            String stavkaQuery = "INSERT INTO Stavka (RacunID,Kolicna,ProitvodID) VALUES(?,?,?)";
+            for (Integer proizvodID : proizvodKolicina.keySet()) {
+                PreparedStatement preparedStatement1 = connection.prepareStatement(stavkaQuery);
+                preparedStatement1.setInt(1,racunId);
+                preparedStatement1.setInt(2,proizvodKolicina.get(proizvodID));
+                preparedStatement1.setInt(3,proizvodID);
+                preparedStatement1.executeUpdate();
+
+            }
+
+
+
+
+
+
+        } catch (SQLException e) {
             e.printStackTrace();
-
         }
+
+
     }
+    public static int DohvacanjeRacunaID(Connection connection , String DatumIzdavanja,int KupacID,int KomercialistID) throws SQLException{
+        String query = "SELECT IDRacun FROM Racun WHERE DatumIzdavanja = ? AND KupacID = ? AND KomercialistID = ?";
 
-    public static void ispisSvihKnjigaZadanogAutora(Connection connection){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite Prezime autora cije knjige zelite pretraziti:");
-        String prezimeAutora = scanner.nextLine();
-
-
-        String query ="SELECT k.id,k.Naslov,a.ime,a.prezime FROM Knjiga AS k INNER JOIN Autor AS a ON a.id = k.autorId WHERE a.Prezime = ?";
-        try {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, prezimeAutora);
-
+            preparedStatement.setString(1,DatumIzdavanja);
+            preparedStatement.setInt(2,KupacID);
+            preparedStatement.setInt(3,KomercialistID);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()){
-                System.out.println("ID Knjige: " +resultSet.getInt("id"));
-                System.out.println("Naslov Knjige: " + resultSet.getString("naslov"));
-                System.out.println("Ime Autora: " + resultSet.getString("ime"));
-                System.out.println("Prezime Autora: " + resultSet.getString("prezime"));
-                System.out.println(" ");
-            }
-            preparedStatement.close();
+            resultSet.next();
+            return resultSet.getInt("IDRacun");
 
 
-        }catch (SQLException e){
-            e.printStackTrace();
 
-        }
+
     }
+    public static String NoviBrojRacuna(Connection connection) throws SQLException {
+        String query = "SELECT MAX(BrojRacuna) FROM Racun";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        resultSet.next();
+        String brojRacunaString= resultSet.getString("BrojRacuna");
+        brojRacunaString.substring(1,6);
 
-    public static void ispisAutoraBezKnjige(Connection connection){
-
-
-        String query ="SELECT a.id,a.ime,a.prezime FROM Autor AS a  LEFT JOIN Knjiga AS k ON k.autorID = A.id  GROUP BY a.id,a.ime,a.prezime  HAVING COUNT(k.autorId) = 0; ";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println("Autori bez unesenih knjiga:");
-            while(resultSet.next()){
-                System.out.println("ID: "+resultSet.getInt("id"));
-                System.out.println("Ime: "+resultSet.getString("ime"));
-                System.out.println("Prezime: "+resultSet.getString("Prezime"));
-                System.out.println(" ");
-
-            }
-            preparedStatement.close();
-
-
-        }catch (SQLException e){
-            e.printStackTrace();
-
-        }
-    }
-
-
-    public static void ispisSvihAutora(Connection connection){
-
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Autor");
-            while(resultSet.next()){
-                System.out.println("id: "+resultSet.getInt("id")+
-                        " Ime: "+resultSet.getString("ime")+
-                        " Prezime: "+resultSet.getString("prezime")+
-                        " Nacionalnost: "+resultSet.getString("nacionalnost"));
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-
-        }
-    }
-    public static void ispisSvihKnjiga(Connection connection){
-        String qury = "SELECT k.id,k.naslov,a.ime,a.prezime FROM Knjiga AS k INNER JOIN Autor AS a ON a.id = k.autorId";
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(qury);
-            while(resultSet.next()){
-                System.out.println("ID Knjige: " +resultSet.getInt("id"));
-                System.out.println("Naslov Knjige: " + resultSet.getString("naslov"));
-                System.out.println("Ime Autora: " + resultSet.getString("ime"));
-                System.out.println("Prezime Autora: " + resultSet.getString("prezime"));
-                System.out.println(" ");
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-
-        }
     }
 
 
--- src/database/DatabaseService.java
+++ src/database/DatabaseService.java
@@ -5,16 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseService {
-    // public , static class razlika je da static mormo krirati instncu
-    //Staticna klasa je kad se atirbuti ne mjenjaju dok je instaca kada se neki atibutei mogu mjenjati pa
-    // se dvije instance razlikuju u nekom segmentu
-    //staticne metode su globalne metode
-    //----------------------------------------------------------------
-    // kriranje paketa za database  i kreiranje klase za spajanje sa bazom
-    //kaoko nebi morali svaki put kreirati
+
     public static Connection CreateConnection(){
-        // URL za povezivanje s bazom podataka
-        String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=Knjiznica;encrypt=true;trustServerCertificate=true";
+
+        String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=AdventureWorksOBP;encrypt=true;trustServerCertificate=true";
         String user = "root";
         String password = "password";
         Connection connection = null;
